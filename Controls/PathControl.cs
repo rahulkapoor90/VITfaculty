320279d06e9dab2e52b64ffe4397e36bf2d11fd4
Rahul Kapoor <rahulkapoorbbps@outlook.com>
2015-03-23 21:50:54 +0530

    VIT Faculty new look
    V2

---
+++ Controls/PathControl.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace VITfaculty.Controls
+{
+    public class PathControl : ContentControl
+    {
+        public PathControl()
+        {
+            DefaultStyleKey = typeof(PathControl);
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Microsoft.Phone.Controls;
+
+namespace VITfaculty
+{
+    public class PathData
+    {
+        public string XAMLContent { get; set; }
+    }
+
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();            
+        }
+    }
+}
